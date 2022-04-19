a9e0284ad3ffb1b81e21b78120d102a2f4346a55
aaaa <a@a.a>
2022-04-19 11:45:08 +0200

    includes et index

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require "includes/head.html";
+require "includes/nav.html";
+?>
+<div class="container-fluid">
+    <article class="col-12 col-md-8 mx-auto py-5">
+    <h1 class="pb-4">Accueil Wikifruit</h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Magni repellendus hic soluta ab at ea delectus consectetur, quos suscipit, necessitatibus,
+        impedit blanditiis! Modi fugiat accusantium repellendus impedit repellat molestias a,
+        dolore ab praesentium recusandae quae debitis facere nesciunt eaque quasi alias iste.
+        Explicabo ad deleniti esse aliquam est? Nam, et.
+    </article>
+</div>
